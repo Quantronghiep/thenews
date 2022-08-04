cb1e49c74790e673a9316edce2941efcb3dc8f7d
Quantronghiep <quantronghiep2001@gmail.com>
2022-08-04 11:26:59 +0700

    add remember me and signup success redirect home

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,16 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserCreateRequest;
 use App\Models\User;
-use Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+
+//use Illuminate\Auth\Events\Registered;
 
 class LoginController extends Controller
 {
     public function check_sign_in(Request $request)
     {
-        $remember = true;
+        $remember = (bool)$request->remember;
         if (Auth::attempt(['email' => $request->email,
             'password' => $request->password,
             'role' => 1,
@@ -39,31 +43,35 @@ class LoginController extends Controller
         return view('login.sign_up');
     }
 
-    public function check_sign_up(Request $request)
+    public function check_sign_up(UserCreateRequest $request)
     {
 //        dd($request);
-        $request->validate([
-            'name' => 'required',
-            'email' => 'required|email',
-            'password' => 'required'
-        ]);
+//        $request->validate([
+//            'name' => 'required',
+//            'email' => 'required|email',
+//            'password' => 'required'
+//        ]);
+
         $user = User::create([
             'name' => $request['name'],
             'email' => $request['email'],
             'password' => Hash::make($request['password']),
             'role' => 0,
         ]);
-        $success = 'Dang ki thanh cong';
+
 //        event(new Registered($user));
         Auth::login($user);
+        $success = 'Dang ki thanh cong';
 
+        return redirect('/')->with('success', $success);
 
-        return redirect('/sign_in')->with('success', $success);
     }
+
 
     public function log_out()
     {
+        Session::flush();
         Auth::logout();
-        return view('login.sign_in');
+        return redirect('/sign_in');
     }
 }
