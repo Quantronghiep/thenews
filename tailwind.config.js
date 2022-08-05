615b660676f8c6d441cc2193e5bd6b71e039f4ab
Quantronghiep <quantronghiep2001@gmail.com>
2022-08-05 10:43:53 +0700

    add pagination and plugin button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
 module.exports = {
     content: [
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
     ],
     theme: {
         extend: {},
@@ -17,5 +19,29 @@ module.exports = {
     },
     plugins: [
         require('@tailwindcss/line-clamp'),
+
+        plugin(function ({addComponents}) {
+            addComponents({
+                '.btn': {
+                    padding: '.5rem 1rem',
+                    borderRadius: '.25rem',
+                    fontWeight: '600',
+                },
+                '.btn-blue': {
+                    backgroundColor: '#3490dc',
+                    color: '#fff',
+                    '&:hover': {
+                        backgroundColor: '#2779bd'
+                    },
+                },
+                '.btn-red': {
+                    backgroundColor: '#e3342f',
+                    color: '#fff',
+                    '&:hover': {
+                        backgroundColor: '#cc1f1a'
+                    },
+                },
+            })
+        })
     ],
 }
